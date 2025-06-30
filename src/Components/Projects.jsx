e00b438465d4799b67491cf09ac52476eebd7497
sharad rakshak <sharad14r@gmail.com>
2025-06-30 12:33:20 +0530

    url changed

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -8,15 +8,15 @@ export default function Projects() {
     <div className='project_title'>
       <h1 className='section_title'>Projects</h1>
       <div className="projects_container">
-        <a href='https://lucky-salmiakki-4c653b.netlify.app/'>
+        <a href='https://lucky-salmiakki-4c653b.netlify.app/' target='_blank'>
             <img className='earth' src={globe} alt="" />
             <h3>Country Finder</h3>
         </a>
-        <a>
+        <a href='https://adorable-bublanina-ac9ea7.netlify.app/' target='_blank'>
             <img className='crud' src={Crud} alt="" />
             <h3>CRUD App</h3>
         </a>
-        <a>
+        <a href='https://gilded-churros-13daa0.netlify.app/' target='_blank'>
             <img className='weather' src={weather} alt="" />
             <h3>Weather Api</h3>
         </a>
